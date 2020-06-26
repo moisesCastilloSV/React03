41ac1d9cf886372fd0594b1e55928d0861ab2380
moises <ldcastil@bancoagricola.com.sv>
2020-06-25 21:29:00 -0600

    Pantalla  Presupuesto
    componente Pregunta

---
+++ src/components/Pregunta.js
@@ -0,0 +1,28 @@
+import React,{Fragment} from 'react';
+const Pregunta = () => {
+    return ( 
+     <Fragment>
+         <h2>Colos tu Presupuesto</h2>
+         <form>
+             <input
+             type="number"
+             className="u-full-width"
+             placeholder="Coloca tu Presupuesto"
+             />
+
+         <input
+           type="submit"
+           className="button-primary u-full-width"
+           value="Definir Presupuesto"
+         />    
+         </form>
+
+
+
+     </Fragment>
+
+
+     );
+}
+ 
+export default Pregunta;
